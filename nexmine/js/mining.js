435b5c2769e5988985304aeea8417c04929919d8
Nexmine <nexmineofficial@gmail.com>
2026-03-08 23:36:03 +0530

    Delete nexmine/js/mining.js

--- nexmine/js/mining.js
+++
@@ -1,9 +0,0 @@
-function generateReferralLink() {
-    const username = localStorage.getItem('nexmine_username') || 'user';
-    const currentUrl = window.location.href;
-    const baseUrl = currentUrl.substring(0, currentUrl.lastIndexOf('/'));
-    const fullLink = `${baseUrl}/signup.html?ref=${username}`;
-    
-    const refInput = document.getElementById('ref-link');
-    if (refInput) refInput.value = fullLink;
-}
